2e192608f2246cb064e22eec8941976e780881d1
sorrelcat <o.schest@gmail.com>
2017-10-24 12:13:29 +0300

    Level 2: define constant for field size, adding abstract class GameObject, variables and methods in it

--- 4.JavaCollections/src/com/javarush/task/task34/task3410/controller/Controller.java
+++ 4.JavaCollections/src/com/javarush/task/task34/task3410/controller/Controller.java
@@ -19,6 +19,6 @@ public class Controller {
     }
 
     public static void main(String [] args) {
-
+        
     }
 }
--- 4.JavaCollections/src/com/javarush/task/task34/task3410/model/Model.java
+++ 4.JavaCollections/src/com/javarush/task/task34/task3410/model/Model.java
@@ -4,4 +4,6 @@ package com.javarush.task.task34.task3410.model;
  * Created by sorre on 24.10.2017.
  */
 public class Model {
+
+    public static final int FIELD_CELL_SIZE = 20;
 }
