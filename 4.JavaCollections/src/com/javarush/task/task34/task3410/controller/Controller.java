d5733715ecf0ceba01204d3c1a31e7f670e4ecb6
sorrelcat <o.schest@gmail.com>
2017-10-24 12:03:08 +0300

    Level 1: adding Model, View, Controller, base variables and constructors

---
+++ 4.JavaCollections/src/com/javarush/task/task34/task3410/controller/Controller.java
@@ -0,0 +1,24 @@
+package com.javarush.task.task34.task3410.controller;
+
+import com.javarush.task.task34.task3410.model.Model;
+import com.javarush.task.task34.task3410.view.View;
+
+/**
+ * Created by sorre on 24.10.2017.
+ */
+public class Controller {
+
+    private View view;
+    private Model model;
+
+    public Controller() {
+
+        this.view = new View(this);
+        this.model = new Model();
+
+    }
+
+    public static void main(String [] args) {
+
+    }
+}
---
+++ 4.JavaCollections/src/com/javarush/task/task34/task3410/view/View.java
@@ -0,0 +1,19 @@
+package com.javarush.task.task34.task3410.view;
+
+import com.javarush.task.task34.task3410.controller.Controller;
+
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * Created by sorre on 24.10.2017.
+ */
+public class View extends JFrame {
+
+    private Controller controller;
+
+    public View(Controller controller) {
+        this.controller = controller;
+    }
+}
+
