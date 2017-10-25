71b67d1afbc960c3949a892f99b1cf2490b15daa
sorrelcat <o.schest@gmail.com>
2017-10-25 12:39:28 +0300

    Level 11: updated Model class: restart level, start new level functions etc.

--- 4.JavaCollections/src/com/javarush/task/task34/task3410/model/Model.java
+++ 4.JavaCollections/src/com/javarush/task/task34/task3410/model/Model.java
@@ -2,6 +2,9 @@ package com.javarush.task.task34.task3410.model;
 
 import com.javarush.task.task34.task3410.controller.EventListener;
 
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 /**
  * Created by sorre on 24.10.2017.
  */
@@ -9,8 +12,31 @@ public class Model {
 
     public static final int FIELD_CELL_SIZE = 20;
     private EventListener eventListener;
+    private GameObjects gameObjects;
+    private int currentLevel = 1;
+    private LevelLoader levelLoader = new LevelLoader(Paths.get("res/levels.txt"));
 
     public void setEventListener(EventListener eventListener) {
         this.eventListener = eventListener;
     }
+
+    public GameObjects getGameObjects() {
+        return gameObjects;
+    }
+
+    public void restartLevel(int level) {
+        this.gameObjects = levelLoader.getLevel(level);
+    }
+
+    public void restart() {
+        restartLevel(currentLevel);
+    }
+
+    public void startNextLevel() {
+        currentLevel++;
+        restart();
+    }
+
+
 }
+
