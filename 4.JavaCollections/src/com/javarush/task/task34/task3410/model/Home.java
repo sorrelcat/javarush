13db8218475923ae776a1e82c84b6e2fe8366b7e
sorrelcat <o.schest@gmail.com>
2017-10-25 11:58:55 +0300

    Level 6: added Home class

---
+++ 4.JavaCollections/src/com/javarush/task/task34/task3410/model/Home.java
@@ -0,0 +1,19 @@
+package com.javarush.task.task34.task3410.model;
+
+import java.awt.*;
+
+/**
+ * Created by sorre on 25.10.2017.
+ */
+public class Home extends GameObject {
+
+    public Home(int x, int y) {
+        super(x, y, 2, 2);
+    }
+
+    @Override
+    public void draw(Graphics graphics) {
+        graphics.setColor(Color.red);
+        graphics.drawOval(getX(), getY(), getWidth(), getHeight());
+    }
+}
